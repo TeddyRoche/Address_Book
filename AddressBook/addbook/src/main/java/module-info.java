a244760212cfd9fde880631892e1baa3e7c1a578
TeddyRoche <90163306+TeddyRoche@users.noreply.github.com>
2023-04-27 10:33:18 -0400

    Delete AddressBook directory

--- AddressBook/addbook/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module addressbook {
-    requires transitive javafx.graphics;
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens addressbook to javafx.fxml;
-    exports addressbook;
-}
